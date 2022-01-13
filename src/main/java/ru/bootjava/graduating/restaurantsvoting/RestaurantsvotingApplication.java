2a8124f6992021c5f91e6d6b3f6c16da72da3911
ry.shumilov <donkarleone08962@gmail.com>
2022-01-14 00:18:30 +0300

    bootjava

---
+++ src/main/java/ru/bootjava/graduating/restaurantsvoting/RestaurantsvotingApplication.java
@@ -0,0 +1,14 @@
+package ru.bootjava.graduating.restaurantsvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RestaurantsvotingApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantsvotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bootjava/graduating/restaurantsvoting/RestaurantsvotingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bootjava.graduating.restaurantsvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantsvotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
