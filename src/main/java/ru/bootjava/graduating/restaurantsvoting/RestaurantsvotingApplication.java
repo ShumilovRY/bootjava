96b31089d3f18ecf894426443450b8892a8d86f9
ry.shumilov <donkarleone08962@gmail.com>
2022-01-20 21:59:43 +0300

    fix bd trouble

--- src/main/java/ru/bootjava/graduating/restaurantsvoting/RestaurantsvotingApplication.java
+++ src/main/java/ru/bootjava/graduating/restaurantsvoting/RestaurantsvotingApplication.java
@@ -1,13 +1,22 @@
 package ru.bootjava.graduating.restaurantsvoting;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.bootjava.graduating.restaurantsvoting.repository.RestaurantRepository;
 
 @SpringBootApplication
-public class RestaurantsvotingApplication {
+@AllArgsConstructor
+public class RestaurantsvotingApplication implements ApplicationRunner {
+
+    private final RestaurantRepository restaurantRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantsvotingApplication.class, args);
     }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception { }
 }
